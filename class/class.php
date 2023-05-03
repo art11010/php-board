b874c946dd66c2b45fb0115390b7e68a260b594d
YEZI <art11010@naver.com>
2023-05-03 22:07:18 +0900

    class 연습

--- class/class.php
+++ class/class.php
@@ -1,7 +1,7 @@
 <?php
     class info{
-        private $_name;
-        public $_age;
+        private $_name; // private : class 밖에서 사용 불가능
+        public $_age;   // public : class 밖에서도 사용 가능
 
         public function setName($_name){
             $this -> _name = $_name; // $this = class info, 객체 자신을 가리키는 키워드
@@ -19,5 +19,6 @@
 
     // var_dump($a);
     // print_r($a);
-    echo $a -> getName();
+
+    echo $a -> getName();   // 클래스에 $aaa 라는 맴버변수가 있다면 $this->aaa 로 접근
 ?>
